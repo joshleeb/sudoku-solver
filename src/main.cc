6467293b9b9f92c9fdb050d72a6355bb64d00258
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-06-27 13:02:06 +1000

    Implement puzzle constructor and display methods

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include "interface.h"
+#include "puzzle.h"
+
 int main() {
-    std::cout << "Work in progress\n";
+    auto initial_board = interface::read_board(std::cin);
+    puzzle sudoku_puzzle(initial_board);
+
+    sudoku_puzzle.display();
+
     return EXIT_SUCCESS;
 }
--- src/puzzle.cc
+++ src/puzzle.cc
@@ -1,18 +1,42 @@
 #include "puzzle.h"
 
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
 #include <vector>
 
 puzzle::puzzle(std::vector<int> board) {
     this->board = board;
     this->fixed_squares = {};
 
-    // TODO: Check board validity.
-    // TODO: Set board width.
-    // TODO: Set fixed squares.
+    this->board_width = std::sqrt(board.size());
+
+    // Set fixed squares.
+    for (int i = 0; i < (int)board.size(); i++) {
+        if (board[i] == BLANK) this->fixed_squares.push_back(i);
+    }
 }
 
 void puzzle::solve() {}
-void puzzle::display() {}
+
+void puzzle::display() {
+    int square;
+
+    for (int row = 0; row < this->board_width; row++) {
+        for (int col = 0; col < this->board_width; col++) {
+            square = this->board[row * this->board_width + col];
+
+            if (square == BLANK) {
+                std::cout << ".";
+            } else {
+                std::cout << square;
+            }
+            std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
 
 bool puzzle::is_valid() {
     return false;
@@ -31,11 +55,11 @@ int puzzle::get_board_width() {
 }
 
 bool puzzle::is_fixed_square(int index) {
-    (void) index;
+    (void)index;
     return false;
 }
 
 std::vector<int> puzzle::get_moves(int index) {
-    (void) index;
+    (void)index;
     return {};
 }
