0df5d452de0b3176d232435c5170123c6247bf6d
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-06-28 16:01:15 +1000

    Add tests for solving the puzzle

--- tests/test_puzzle.cc
+++ tests/test_puzzle.cc
@@ -18,6 +18,42 @@ SCENARIO("constructing a puzzle", "[puzzle]") {
     }
 }
 
+SCENARIO("solving a puzzle", "[puzzle]") {
+    GIVEN("a puzzle with a 4x4 board") {
+        puzzle new_puzzle(std::vector<int>{2, 1, -1, -1, 4, -1, -1, 1, 3, -1, -1,
+                                           4, -1, -1, -1, 2});
+
+        THEN("should solve the puzzle") {
+            new_puzzle.solve();
+
+            std::vector<int> expected_board = {2, 1, 4, 3, 4, 3, 2, 1,
+                                               3, 2, 1, 4, 1, 4, 3, 2};
+            REQUIRE(new_puzzle.get_board() == expected_board);
+        }
+    }
+
+    GIVEN("a 9x9 board") {
+        puzzle new_puzzle(std::vector<int>{
+            5,  3,  -1, -1, 7,  -1, -1, -1, -1, 6,  -1, -1, 1,  9,  5,  -1, -1,
+            -1, -1, 9,  8,  -1, -1, -1, -1, 6,  -1, 8,  -1, -1, -1, 6,  -1, -1,
+            -1, 3,  4,  -1, -1, 8,  -1, 3,  -1, -1, 1,  7,  -1, -1, -1, 2,  -1,
+            -1, -1, 6,  -1, 6,  -1, -1, -1, -1, 2,  8,  -1, -1, -1, -1, 4,  1,
+            9,  -1, -1, 5,  -1, -1, -1, -1, 8,  -1, -1, 7,  9,
+        });
+
+        THEN("should solve the puzzle") {
+            new_puzzle.solve();
+
+            std::vector<int> expected_board = {
+                5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8,
+                3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3,
+                7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4,
+                2, 8, 7, 4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9};
+            REQUIRE(new_puzzle.get_board() == expected_board);
+        }
+    }
+}
+
 SCENARIO("checking if puzzle is valid", "[puzzle]") {
     GIVEN("a valid board") {
         puzzle new_puzzle(std::vector<int>{2, 1, -1, -1, -1, 3, 2, -1, -1, -1,
--- tests/test_puzzle_private.cc
+++ tests/test_puzzle_private.cc
@@ -6,92 +6,92 @@
 #include "../src/puzzle.h"
 
 SCENARIO("getting available moves", "[puzzle]") {
-    // GIVEN("a puzzle with a valid 9x9 board") {
-    //     std::vector<int> expected_moves{};
-    //     puzzle new_puzzle(std::vector<int>{
-    //         5,  3,  -1, -1, 7,  -1, -1, -1, -1, 6,  -1, -1, 1,  9,  5,  -1, -1,
-    //         -1, -1, 9,  8,  -1, -1, -1, -1, 6,  -1, 8,  -1, -1, -1, 6,  -1, -1,
-    //         -1, 3,  4,  -1, -1, 8,  -1, 3,  -1, -1, 1,  7,  -1, -1, -1, 2,  -1,
-    //         -1, -1, 6,  -1, 6,  -1, -1, -1, -1, 2,  8,  -1, -1, -1, -1, 4,  1,
-    //         9,  -1, -1, 5,  -1, -1, -1, -1, 8,  -1, -1, 7,  9,
-    //     });
+    GIVEN("a puzzle with a valid 9x9 board") {
+        std::vector<int> expected_moves{};
+        puzzle new_puzzle(std::vector<int>{
+            5,  3,  -1, -1, 7,  -1, -1, -1, -1, 6,  -1, -1, 1,  9,  5,  -1, -1,
+            -1, -1, 9,  8,  -1, -1, -1, -1, 6,  -1, 8,  -1, -1, -1, 6,  -1, -1,
+            -1, 3,  4,  -1, -1, 8,  -1, 3,  -1, -1, 1,  7,  -1, -1, -1, 2,  -1,
+            -1, -1, 6,  -1, 6,  -1, -1, -1, -1, 2,  8,  -1, -1, -1, -1, 4,  1,
+            9,  -1, -1, 5,  -1, -1, -1, -1, 8,  -1, -1, 7,  9,
+        });
 
-    //     THEN("should return available moves") {
-    //         expected_moves = {1, 2, 4};
-    //         REQUIRE(new_puzzle.get_moves(2) == expected_moves);
+        THEN("should return available moves") {
+            expected_moves = {1, 2, 4};
+            REQUIRE(new_puzzle.get_moves(2) == expected_moves);
 
-    //         expected_moves = {2, 6};
-    //         REQUIRE(new_puzzle.get_moves(3) == expected_moves);
+            expected_moves = {2, 6};
+            REQUIRE(new_puzzle.get_moves(3) == expected_moves);
 
-    //         expected_moves = {2, 4, 6, 8};
-    //         REQUIRE(new_puzzle.get_moves(5) == expected_moves);
+            expected_moves = {2, 4, 6, 8};
+            REQUIRE(new_puzzle.get_moves(5) == expected_moves);
 
-    //         expected_moves = {1, 4, 8, 9};
-    //         REQUIRE(new_puzzle.get_moves(6) == expected_moves);
+            expected_moves = {1, 4, 8, 9};
+            REQUIRE(new_puzzle.get_moves(6) == expected_moves);
 
-    //         expected_moves = {1, 2, 4, 9};
-    //         REQUIRE(new_puzzle.get_moves(7) == expected_moves);
+            expected_moves = {1, 2, 4, 9};
+            REQUIRE(new_puzzle.get_moves(7) == expected_moves);
 
-    //         expected_moves = {2, 4, 8};
-    //         REQUIRE(new_puzzle.get_moves(8) == expected_moves);
-    //     }
+            expected_moves = {2, 4, 8};
+            REQUIRE(new_puzzle.get_moves(8) == expected_moves);
+        }
 
-    //     WHEN("having already tried options") {
-    //         new_puzzle.attempts[7] = std::vector<int>{1, 2, 4};
+        WHEN("having already tried options") {
+            new_puzzle.attempts[7] = std::vector<int>{1, 2, 4};
 
-    //         THEN("should return available moves without tries options") {
-    //             expected_moves = {9};
-    //             REQUIRE(new_puzzle.get_moves(7) == expected_moves);
-    //         }
-    //     }
-    // }
+            THEN("should return available moves without tries options") {
+                expected_moves = {9};
+                REQUIRE(new_puzzle.get_moves(7) == expected_moves);
+            }
+        }
+    }
 
-    // GIVEN("a puzzle with a valid 4x4 board") {
-    //     std::vector<int> expected_moves{};
-    //     puzzle new_puzzle(std::vector<int>{2, 1, -1, -1, -1, 3, 2, -1, -1, -1,
-    //                                        -1, 4, 1, -1, -1, -1});
+    GIVEN("a puzzle with a valid 4x4 board") {
+        std::vector<int> expected_moves{};
+        puzzle new_puzzle(std::vector<int>{2, 1, -1, -1, -1, 3, 2, -1, -1, -1,
+                                           -1, 4, 1, -1, -1, -1});
 
-    //     THEN("should return available moves") {
-    //         expected_moves = {3, 4};
-    //         REQUIRE(new_puzzle.get_moves(2) == expected_moves);
+        THEN("should return available moves") {
+            expected_moves = {3, 4};
+            REQUIRE(new_puzzle.get_moves(2) == expected_moves);
 
-    //         expected_moves = {3};
-    //         REQUIRE(new_puzzle.get_moves(3) == expected_moves);
+            expected_moves = {3};
+            REQUIRE(new_puzzle.get_moves(3) == expected_moves);
 
-    //         expected_moves = {4};
-    //         REQUIRE(new_puzzle.get_moves(4) == expected_moves);
+            expected_moves = {4};
+            REQUIRE(new_puzzle.get_moves(4) == expected_moves);
 
-    //         expected_moves = {1};
-    //         REQUIRE(new_puzzle.get_moves(7) == expected_moves);
+            expected_moves = {1};
+            REQUIRE(new_puzzle.get_moves(7) == expected_moves);
 
-    //         expected_moves = {3};
-    //         REQUIRE(new_puzzle.get_moves(8) == expected_moves);
+            expected_moves = {3};
+            REQUIRE(new_puzzle.get_moves(8) == expected_moves);
 
-    //         expected_moves = {2};
-    //         REQUIRE(new_puzzle.get_moves(9) == expected_moves);
+            expected_moves = {2};
+            REQUIRE(new_puzzle.get_moves(9) == expected_moves);
 
-    //         expected_moves = {1, 3};
-    //         REQUIRE(new_puzzle.get_moves(10) == expected_moves);
+            expected_moves = {1, 3};
+            REQUIRE(new_puzzle.get_moves(10) == expected_moves);
 
-    //         expected_moves = {2, 4};
-    //         REQUIRE(new_puzzle.get_moves(13) == expected_moves);
+            expected_moves = {2, 4};
+            REQUIRE(new_puzzle.get_moves(13) == expected_moves);
 
-    //         expected_moves = {3};
-    //         REQUIRE(new_puzzle.get_moves(14) == expected_moves);
+            expected_moves = {3};
+            REQUIRE(new_puzzle.get_moves(14) == expected_moves);
 
-    //         expected_moves = {2, 3};
-    //         REQUIRE(new_puzzle.get_moves(15) == expected_moves);
-    //     }
+            expected_moves = {2, 3};
+            REQUIRE(new_puzzle.get_moves(15) == expected_moves);
+        }
 
-    //     WHEN("having already tried options") {
-    //         new_puzzle.attempts[2] = std::vector<int>{3};
+        WHEN("having already tried options") {
+            new_puzzle.attempts[2] = std::vector<int>{3};
 
-    //         THEN("should return available moves without tries options") {
-    //             expected_moves = {4};
-    //             REQUIRE(new_puzzle.get_moves(2) == expected_moves);
-    //         }
-    //     }
-    // }
+            THEN("should return available moves without tries options") {
+                expected_moves = {4};
+                REQUIRE(new_puzzle.get_moves(2) == expected_moves);
+            }
+        }
+    }
 }
 
 SCENARIO("getting index from row and column", "[puzzle]") {
